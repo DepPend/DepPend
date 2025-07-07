6ca63c228698ee5e174948aba1bd2f618580631d
DepPend <departemenpendidikanhimafkom@gmail.com>
2025-07-07 22:07:31 +0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -47,7 +47,7 @@ export default function AboutPage() {
 const styles = {
   heroSection: {
     minHeight: '100vh',
-    backgroundImage: 'url("/hima4.jpg")',
+    backgroundImage: 'url("../hima4.JPG")',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
