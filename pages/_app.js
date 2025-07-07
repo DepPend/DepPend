8a801925e368bbb947995af57dab3e73d70d8e25
DepPend <departemenpendidikanhimafkom@gmail.com>
2025-07-07 22:28:37 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,22 @@
+// pages/_app.js
 import '../styles/globals.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
-import Head from 'next/head'; // ✅ import Head
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => (
     <>
       <Head>
         <title>HIMA F.KOM</title>
-        <link rel="icon" href="../hima5.PNG" /> {/* ✅ pasang favicon */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="../hima5.PNG" />
       </Head>
       <Header />
-      <div style={{
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-      }}>
-        <main style={{ flex: 1 }}>{page}</main>
+      <div className="min-h-screen flex flex-col">
+        <main className="flex-1 px-4 sm:px-6 md:px-8 lg:px-16 xl:px-20">
+          {page}
+        </main>
         <Footer />
       </div>
     </>
