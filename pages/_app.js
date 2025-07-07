33998532a1778ed45f475cfd87f336ca842acedd
DepPend <departemenpendidikanhimafkom@gmail.com>
2025-07-07 22:03:24 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Head from 'next/head'; // ✅ import Head
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => (
     <>
+      <Head>
+        <title>HIMA F.KOM</title>
+        <link rel="icon" href="/hima5.png" /> {/* ✅ pasang favicon */}
+      </Head>
       <Header />
       <div style={{
         minHeight: '100vh',
