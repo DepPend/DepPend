4b0733f43872d4a31d36187f5f32b640b6a25d98
DepPend <departemenpendidikanhimafkom@gmail.com>
2025-09-08 20:19:19 +0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,24 @@
 export default function Footer() {
   return (
     <footer style={styles.footer}>
-      <p>&copy; 2025 Departemen Pendidikan. Semua Hak Dilindungi.</p>
+      <p>&copy; 2025 Departemen Pendidikan.</p>
+      <p>Himpunan Mahasiswa Fakultas Komputer</p>
+      <p>Semua Hak Dilindungi.</p>
     </footer>
   );
 }
 
 const styles = {
   footer: {
-    background: 'rgba(5, 56, 143)', // hitam transparan
+    background: 'rgba(5, 56, 143)',
     color: 'white',
     padding: '1rem',
     textAlign: 'center',
-    marginTop: 'auto', // dorong ke bawah jika pakai flex wrapper
-    backdropFilter: 'blur(2px)', // optional efek blur
+    marginTop: 'auto',
+    backdropFilter: 'blur(2px)',
     position: 'relative',
     bottom: 0,
     width: '100%',
+    lineHeight: '1.6',
   },
 };
